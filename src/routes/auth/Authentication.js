b50b9c43590f8551ae026a5d3a36389c3d1553d5
Louvani <paula.louvani@gmail.com>
2022-08-20 13:13:17 -0500

    manage authentication

--- src/routes/auth/Authentication.js
+++ src/routes/auth/Authentication.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
 import { useState } from 'react';
 import Button from '../../components/general/Button';
+
 import Login from './Login';
 import Register from './Register';
+
 import './auth.css';
+import { URL_API } from '../../App';
 
 
 function Authentication({setIsLoggedIn, setUserUsername}) {
@@ -14,8 +18,45 @@ function Authentication({setIsLoggedIn, setUserUsername}) {
         const id = event.target.getAttribute('id')
         id === 'signUp' ? set_switch(false) : set_switch(true)
     }
+
+    const handleSubmit = (onSubmit) => {
+        onSubmit.preventDefault();
+        if (_switch) {
+            axios.post(
+            `${URL_API}/api/auth/login`,
+            {username, password},
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            })
+            .then((res) => res.data)
+            .then((data) => {
+              setIsLoggedIn(true);
+              setUserUsername(data.username)
+              localStorage.setItem('accessToken', data.accessToken);
+            })
+            .catch((error) => console.log(error))
+        } else {
+            axios.post(
+            `${URL_API}/api/auth/register`,
+            {username, password},
+            {
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            })
+            .then((res) => res.data)
+            .then((data) => {
+                setIsLoggedIn(true);
+                setUserUsername(data.username)
+                localStorage.setItem('accessToken', data.accessToken);
+            })
+            .catch((error) => console.log(error))
+        }
+    }
     return(
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
             <div className='header-auth'>
                 <Button label={'button'} className={_switch ? 'btn-light' : 'btn-dark'} onClick={btnClick} text={'Sing In'} id={'signIn'} />
                 <Button label={'button'} className={_switch ? 'btn-dark' : 'btn-light'} onClick={btnClick} text={'Sing up'} id={'signUp'} />
