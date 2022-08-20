69a46297511eefe44a097cd5beb7ed19894bd582
Louvani <paula.louvani@gmail.com>
2022-08-20 04:24:56 -0500

    Initialize app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import './App.css';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export const URL_API = 'http://localhost:8000'
+
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userUsername, setUserUsername] = useState("")
+
+  useEffect(() => {
+    const accessToken = window.localStorage.getItem('accessToken');
+    axios.post(
+      `${URL_API}/api/auth/`,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((res) => res.data)
+      .then((data) => {
+        setIsLoggedIn(true);
+        setUserUsername(data.username)
+      })
+      .catch((error) => console.log(error))
+  })
   return (
-    <div className="App">
+    <div className="App light-section">
+      {!isLoggedIn ? (
+        <h1>Authentication</h1>
+      ) : (
+        <h1>Dashboard</h1>
+      )}
     </div>
   );
 }
