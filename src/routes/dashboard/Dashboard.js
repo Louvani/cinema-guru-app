a97d4fef969a946ad841f8f18aef4a4963c6a869
Louvani <paula.louvani@gmail.com>
2022-08-21 18:41:48 -0500

    Dashboard routing

--- src/routes/dashboard/Dashboard.js
+++ src/routes/dashboard/Dashboard.js
@@ -10,6 +10,10 @@ function Dashboard({ userUsername, setIsLoggedIn }) {
             <div className="Dashboard">
                 <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
                 <SideBar />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/favorites" element={<Favorites />} />
+                <Route path="/watchlater" element={<WatchLater />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </div>
         </BrowserRouter>
     )
